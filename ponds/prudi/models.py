8c5533a52c87dbd031ceec4de6928878a7a8d04c
Elnur <elnurik97qw@yandex.ru>
2023-03-01 23:23:16 +0300

    Last commit on this day motherfucker

--- ponds/prudi/models.py
+++ ponds/prudi/models.py
@@ -3,3 +3,7 @@ from django.db import models
 class Ponds(models.Model):
     title = models.CharField(max_length=255)
     photo = models.ImageField(upload_to="photo/")
+
+class Hoods(models.Model):
+    title = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to="photo/")
