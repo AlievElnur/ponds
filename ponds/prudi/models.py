d79fcac7b65b0727283e7c03043db345fef03a62
Elnur <elnurik97qw@yandex.ru>
2023-03-01 21:52:31 +0300

    last Fix

--- ponds/prudi/models.py
+++ ponds/prudi/models.py
@@ -3,19 +3,3 @@ from django.db import models
 class Ponds(models.Model):
     title = models.CharField(max_length=255)
     photo = models.ImageField(upload_to="photo/")
-
-class Ponds(models.Model):
-    title = models.CharField(max_length=255)
-    photo = models.ImageField(upload_to="photo/")
-    
-class RRRRRR(models.Model):
-    title = models.CharField(max_length=255)
-    photo = models.ImageField(upload_to="photo/")
-    
-class RRRRRR(models.Model):
-    title = models.CharField(max_length=255)
-    photo = models.ImageField(upload_to="photo/")
-
-class RRRRRR(models.Model):
-    title = models.CharField(max_length=255)
-    photo = models.ImageField(upload_to="photo/")
