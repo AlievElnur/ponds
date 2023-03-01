afdad847beb832af5d3753fff8076a30c47ee08f
Elnur <elnurik97qw@yandex.ru>
2023-03-01 21:36:42 +0300

    Test Fix123

--- ponds/prudi/models.py
+++ ponds/prudi/models.py
@@ -3,3 +3,11 @@ from django.db import models
 class Ponds(models.Model):
     title = models.CharField(max_length=255)
     photo = models.ImageField(upload_to="photo/")
+
+class Yolo(models.Model):
+    title = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to="photo/")
+
+class Hoods(models.Model):
+    title = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to="photo/")
