4c7e395e7ffe1ed2d4c32db82bf65bbf7cc77b9f
GonnaMagicJS <arrivedercijs@gmail.com>
2023-03-02 14:59:49 +0300

    Демо сайт ласт версия

--- ponds/prudi/static/prudi/js/demo.js
+++ ponds/prudi/static/prudi/js/demo.js
@@ -25,3 +25,36 @@ window.onscroll = function() {
 }
 
 window.scrollTo(0, 1)
+
+// Audio
+
+let soundButton = document.querySelector('.soundbutton'),
+    audio = document.querySelector('.audio')
+
+soundButton.addEventListener('click', e => {
+    soundButton.classList.toggle('paused')
+    audio.paused ? audio.play() : audio.pause()
+})
+
+window.onfocus = function() {
+    soundButton.classList.contains('paused') ? audio.pause() : audio.play()
+}
+
+window.onblur = function () {
+    audio.pause()
+}
+
+// Fullscreen
+
+document.addEventListener('click', function(event) {
+    if( !event.target.hasAttribute('data-toggle-fullscreen') ) return;
+}, false);
+
+document.addEventListener('click', function(event) {
+    if (!event.target.hasAttribute('data-toggle-fullscreen')) return;
+    if (document.fullscreenElement) {
+        document.exitFullscreen();
+    } else {
+        document.documentElement.requestFullscreen();
+    }
+}, false);
