aad63ebe8a60b84f4a15f1aac6959d01abd37e5d
Elnur <elnurik97qw@yandex.ru>
2023-03-03 14:42:09 +0300

    админ панель

--- ponds/prudi/admin.py
+++ ponds/prudi/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+class FishAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'photo')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
+
+class SculpturesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'photo')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
+
+class ChannelsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'photo')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
+
+class PlantsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'photo')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
+
+class OtherAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'photo')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')    
+
+admin.site.register(Fish, FishAdmin)
+admin.site.register(Sculptures, SculpturesAdmin)
+admin.site.register(Channels, ChannelsAdmin)
+admin.site.register(Plants, PlantsAdmin)
+admin.site.register(Other, OtherAdmin)
--- ponds/prudi/models.py
+++ ponds/prudi/models.py
@@ -5,22 +5,37 @@ class Fish(models.Model):
     photo = models.ImageField(upload_to="photo/")
     content = models.TextField(blank=True)
 
+    def __str__(self):
+        return self.title
+
 class Sculptures(models.Model):
     title = models.CharField(max_length=255)
     photo = models.ImageField(upload_to="photo/")
     content = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
 
 class Channels(models.Model):
     title = models.CharField(max_length=255)
     photo = models.ImageField(upload_to="photo/")
     content = models.TextField(blank=True)
 
+    def __str__(self):
+        return self.title
+
 class Plants(models.Model):
     title = models.CharField(max_length=255)
     photo = models.ImageField(upload_to="photo/")
     content = models.TextField(blank=True)
 
+    def __str__(self):
+        return self.title
+
 class Other(models.Model):
     title = models.CharField(max_length=255)
     photo = models.ImageField(upload_to="photo/")
     content = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
