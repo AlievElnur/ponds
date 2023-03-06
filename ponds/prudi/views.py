b49fe74407496a7ff86f9b822426c8b2b9485261
Elnur <elnurik97qw@yandex.ru>
2023-03-06 16:05:52 +0300

    EBANIY ROT POLY4ILOC'

--- ponds/prudi/views.py
+++ ponds/prudi/views.py
@@ -1,6 +1,34 @@
-from django.shortcuts import render
+import dataclasses
+from django.shortcuts import redirect, render
 from django.views.generic import *
+
+from .forms import ContactForm
 from .models import *
+
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+
+
+def contact_form(request):
+    form = ContactForm()
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data['name'])
+            print(form.cleaned_data['phone'])
+            print(request.POST.get('url_from'))
+
+            data = {
+                'name': form.cleaned_data['name'],
+                'phone': form.cleaned_data['phone']
+            }
+        
+            html_body = render_to_string('email_templates/contact.html', data)
+            msg = EmailMultiAlternatives(subject='Звонок', from_email='elnurik97qw@yandex.ru', to=['elnurik97qw@yandex.ru'])
+            msg.attach_alternative(html_body, "text/html")
+            msg.send()
+
+    return redirect(request.POST.get('url_from'))
 
 
 class HomePage(TemplateView):
