7e2b3185706077e3fdd51cdd9c1e9f1a1888c6aa
Elnur <elnurik97qw@yandex.ru>
2023-03-11 18:37:13 +0300

    qwe

--- ponds/ponds/settings.py
+++ ponds/ponds/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'prudi.apps.PrudiConfig',
 ]
 
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'ponds.urls'
@@ -136,3 +138,7 @@ EMAIL_HOST_USER = "elnurik97qw@yandex.ru"
 EMAIL_HOST_PASSWORD = "ovelvsbimkzliyjm"
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
